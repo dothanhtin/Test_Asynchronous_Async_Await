8e0d2ace320bf486df828494d4b1bca9113935da
dotha <dotha@THREAD>
2020-08-28 16:16:05 +0700

    first commit

---
+++ Understand_Await_Async_Csharp/Program.cs
@@ -0,0 +1,123 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Understand_Await_Async_Csharp
+{
+    class Program
+    {
+        #region old code test
+        //static async Task Main(string[] args)
+        //{
+        //    Console.WriteLine($"{' ',5} {Thread.CurrentThread.ManagedThreadId,3} MainThread");
+        //    //Task<string> t1 = Program.Async1("A", "B");
+        //    //Task t2 = Program.Async2();
+
+        //    var t1 = Program.Async1("x", "y");
+        //    var t2 = Program.Async2();
+        //    Console.WriteLine("Làm gì đó ở thread chính sau khi 2 task chạy");
+
+
+        //    // Làm gì đó khi t1, t2 đang chạy
+        //    Console.WriteLine("Task1, Task2 đang chạy");
+
+
+        //    await t1; // chờ t1 kết thúc
+        //    Console.WriteLine("Làm gì đó khi t1 kết thúc");
+
+        //    // Chờ t1 kết thúc và đọc kết quả trả về
+        //    //t1.Wait();
+        //    //String s = t1.Result;
+        //    //Program.WriteLine(s, ConsoleColor.Red);
+
+        //    // Ngăn không cho thread chính kết thúc
+        //    // Nếu thread chính kết thúc mà t2 đang chạy nó sẽ bị ngắt
+        //    Console.ReadKey();
+        //}
+        #endregion
+        static async Task Main(string[] args)
+        {
+            Console.WriteLine("Run multi tasks:");
+            //var task1 = TestTask.printTask1(10);
+            //var task2 = TestTask.printTask2(9);
+
+            //await Task.WhenAll(task1, task2); //run bất đồng bộ cho cả 2 task
+            
+            var task1 = TestTask.printTask1(9);
+            //await task1;
+            var task2 = TestTask.printTask2(10);
+            //await task2;
+            var task3 = TestTask.printTask3(11);
+            var task4 = TestTask.printTask4(12);
+            var task5 = TestTask.printTask5(13);
+            var task6 = TestTask.printTask6(14);
+            await Task.WhenAll(task1, task2, task3, task4, task5, task6);
+            //await task2;
+            //await TestTask.printTask1(10);
+            //await TestTask.printTask2(9);
+            Console.ReadKey();
+        }
+        // Viết ra màn hình thông báo có màu
+        public static void WriteLine(string s, ConsoleColor color)
+        {
+            Console.ForegroundColor = color;
+            Console.WriteLine(s);
+        }
+
+        // Tạo và chạy Task, sử dụng delegate Func (có kiểu trả về)
+        public async static Task<string> Async1(string thamso1, string thamso2)
+        {
+            // tạo biến delegate trả về kiểu string, có một tham số object
+            Func<object, string> myfunc = (object thamso) =>
+            {
+                // Đọc tham số (dùng kiểu động - xem kiểu động để biết chi tiết)
+                dynamic ts = thamso;
+                for (int i = 1; i <= 10; i++)
+                {
+                    //  Thread.CurrentThread.ManagedThreadId  trả về ID của thread đạng chạy
+                    WriteLine($"{i,5} {Thread.CurrentThread.ManagedThreadId,3} Tham số {ts.x} {ts.y}",
+                        ConsoleColor.Green);
+                    Thread.Sleep(500);
+                }
+                return $"Kết thúc Async1! {ts.x}";
+            };
+
+            Task<string> task = new Task<string>(myfunc, new { x = thamso1, y = thamso2 });
+            task.Start(); // chạy thread
+
+            // Làm gì đó sau khi chạy Task ở đây
+            Console.WriteLine("Async1: Làm gì đó sau khi task chạy");
+
+            //string ketqua = task.Result;   // khóa (block) thread cha - chờ task hoàn thành
+            await task;
+
+            // Từ đây là code sau await (trong Async1) sẽ chỉ thi hành khi task kết thúc
+            string ketqua = task.Result;
+            Console.WriteLine("Làm gì đó khi task 1 đã kết thúc");
+            //return task;
+            return ketqua;
+        }
+
+        // Tạo và chạy Task, sử dụng delegate Action (không kiểu trả về)
+        public static Task Async2()
+        {
+            Action myaction = () =>
+            {
+                for (int i = 1; i <= 10; i++)
+                {
+                    WriteLine($"{i,5} {Thread.CurrentThread.ManagedThreadId,3}",
+                        ConsoleColor.Yellow);
+                    Thread.Sleep(2000);
+                }
+            };
+            Task task = new Task(myaction);
+            task.Start();
+
+            // Làm gì đó sau khi chạy Task ở đây
+            Console.WriteLine("Async2: Làm gì đó sau khi task 2 chạy");
+
+            return task;
+        }
+
+    }
+}
---
+++ Understand_Await_Async_Csharp/TestTask.cs
@@ -0,0 +1,113 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Understand_Await_Async_Csharp
+{
+    public class TestTask
+    {
+        //task1
+        public async static Task printTask1(int soLanLap1)
+        {
+            Action action1 = () =>
+            {
+                for (int i = 0; i <= soLanLap1; i++)
+                {
+                    WriteLine($"Task1 - {i}", ConsoleColor.Green);
+                    Thread.Sleep(100);
+                }
+            };
+            var task1 = new Task(action1);
+            task1.Start();
+            await task1;
+            WriteLine($"Ket thuc task1", ConsoleColor.Green);
+        }
+
+        //task2
+        public async static Task printTask2(int soLanLap2)
+        {
+            Action action2 = () =>
+            {
+                for (int i = 0; i <= soLanLap2; i++)
+                {
+                    WriteLine($"Task2 - {i}", ConsoleColor.Yellow);
+                    Thread.Sleep(200);
+                }
+            };
+            var task2 = new Task(action2);
+            task2.Start();
+            await task2;
+            WriteLine($"Ket thuc task2", ConsoleColor.Yellow);
+        }
+
+        //task3
+        public async static Task<string> printTask3(int soLanLap3)
+        {
+            Func<int, string> func3 = (int time) =>
+             {
+                 for (int i = 0; i <= time; i++)
+                 {
+                     WriteLine($"Task3 - {i}", ConsoleColor.Cyan);
+                     Thread.Sleep(300);
+                 }
+                 return $"Ket thuc func3";
+             };
+            var task3 = new Task<string>(() => func3(soLanLap3));
+            task3.Start();
+            await task3;
+            string res = task3.Result;
+            WriteLine(res, ConsoleColor.Cyan);
+            return task3.Result;
+        }
+        //task4
+        public async static Task<string> printTask4(int soLanLap4)
+        {
+            Func<int, string> func4 = (int time) =>
+               {
+                   for (int i = 0; i <= time; i++)
+                   {
+                       WriteLine($"Task4 - {i}", ConsoleColor.White);
+                       Thread.Sleep(400);
+                   }
+                   return $"Ket thuc func4";
+               };
+            var task4 = new Task<string>(() => func4(soLanLap4));
+            task4.Start();
+            await task4;
+            var res = task4.Result;
+            WriteLine(res, ConsoleColor.White);
+            return res;
+        }
+
+        //-- phần này không phải là bất đồng bộ, vẫn thực hiện như đồng bộ
+        //task5
+        public static Task printTask5(int soLanLap5)
+        {
+            for (int i = 0; i <= soLanLap5; i++)
+            {
+                WriteLine($"Task5 - {i}", ConsoleColor.Red);
+                Thread.Sleep(500);
+            }
+            WriteLine($"Ket thuc task5", ConsoleColor.Red);
+            return Task.FromResult(1);
+        }
+        //task6
+        public static Task printTask6(int soLanLap6)
+        {
+            for (int i = 0; i <= soLanLap6; i++)
+            {
+                WriteLine($"Task6 - {i}", ConsoleColor.DarkMagenta);
+                Thread.Sleep(600);
+            }
+            WriteLine($"Ket thuc task6", ConsoleColor.DarkMagenta);
+            return Task.FromResult(1);
+        }
+        public static void WriteLine(string s, ConsoleColor color)
+        {
+            Console.ForegroundColor = color;
+            Console.WriteLine(s);
+        }
+    }
+}
